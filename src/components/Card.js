47328b77af482fe226c8ca1abfc75187fc306685
NITHYANANTHAN K <nithyananthank8@gmail.com>
2024-11-05 08:59:30 +0530

    Modified card component
    changed card component and Home component styling. Added assets directory
    and added images in that

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,39 @@
 import React from 'react'
 
-function Card() {
+function Card({character}) {
     const styles={
         card:{
-            width:"100px",
-            height:"auto",
-            backgroundColor:"red"
+            width:"300px",
+            height:"320px",
+            border:"1px solid #ddd",
+            borderRadius:"10px",
+            boxShadow:"0 0 10px rgba(0,0,0,0.1)"
+        },
+        card_img:{
+            width:"100%",
+            height:"250px",
+            objectFit:"cover",
+            borderTopLeftRadius:"10px",
+            borderTopRightRadius:"10px"
+        },
+        card_body:{
+            //padding:"0px",
+            textAllign:"center",
+            //marginBottom:"20px"
+        },
+        card_title:{
+            fontWeight:"bold",
+            fontSize:"18px",
+            
         }
     }
   return (
     <div>
         <div style={styles.card}>
-            sdjfjdgfesdijkdhfjdhfjkhdfkgjhdghdfgkjgggkdfhkjgfhdgkjfdggfgjgjk
+            <img src={character.image} alt={character.name} style={styles.card_img}/>
+            <div style={styles.card_body}>
+                <h5 style={styles.card_title}>{character.name}</h5>
+            </div>
         </div>    
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
-
+import './style.css'
+const characters=[
+  {id:1,name:"Kakashi Hatake",image:"/assets/1.jpeg"},
+  {id:2,name:"Kakashi Hatake",image:"/assets/2.jpeg"},
+  {id:3,name:"Kakashi Hatake",image:"/assets/3.jpeg"},
+  {id:4,name:"Kakashi Hatake",image:"/assets/4.jpeg"},
+  {id:5,name:"Kakashi Hatake",image:"/assets/5.jpg"},
+  {id:6,name:"Kakashi Hatake",image:"/assets/6.jpeg"},
+  {id:7,name:"Kakashi Hatake",image:"/assets/7.jpeg"}
+]
 function Home() {
   return (
     <div> 
-      <div className='nav'> <Navbar/></div>
-      <center>hi hello vannakam vanthanam namste and nasmkar</center>
-      <center>
-        <Card/>
-        </center>
+      <Navbar/>
+      <div className='container'>
+        <div className='row'>
+          {characters.map((character)=>(<div key={character.id} className='col-md-4'><Card character={character}/></div>))}
+        </div>
+      </div>
     </div>
   )
 }
